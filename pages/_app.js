a61c0f5c2edc4f83f78de9b6963bfced95f15495
yogevzipori <yogevzipori@gmail.com>
2023-01-12 17:44:25 -0800

    page to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Page from "../app/page";
 import '../styles/globals.css'
 import RootLayout from '../app/layout.js'
 
@@ -7,6 +7,7 @@ import RootLayout from '../app/layout.js'
 export default function App({ Component, pageProps }) {
   return(
 <RootLayout>
+  <Page/>
  <Component {...pageProps} />
    
    </RootLayout>
