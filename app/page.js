6496e51acf42c2bef03c8bdf4f915876cff4e499
yogevzipori <yogevzipori@gmail.com>
2023-01-12 18:59:51 -0800

    useRouter

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import Home from '/pages/home.js'
 
 
 function Page() {
+  const router = useRouter();
   return (
     <div>
       <Home />
