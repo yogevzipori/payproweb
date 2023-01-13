a80d50d3ed6f10b69a2aae67b9002e53ca8663a6
yogevzipori <yogevzipori@gmail.com>
2023-01-12 20:20:03 -0800

    errors

--- pages/about.js
+++ pages/about.js
@@ -15,13 +15,13 @@ export default function About() {
               Leads and marketing is our specialty
             </div>
             <div className="mx-auto mt-6 max-w-2xl text-md leading-7 text-gray-600">
-            At LeadPro Media LLC, we understand the importance of generating leads and driving conversions for your business. That's why we are dedicated to providing the most effective digital marketing solutions for businesses of all sizes and industries.
+            At LeadPro Media LLC, we understand the importance of generating leads and driving conversions for your business. This is why we are dedicated to providing the most effective digital marketing solutions for businesses of all sizes and industries.
 
 Our team of experts are passionate about what they do, and utilize the latest technologies and data analysis techniques to identify and target the most valuable leads for our clients. We take the time to understand your unique business objectives and tailor our approach to fit your specific needs.
 
 Our services include high-quality direct traffic acquisition, advanced network traffic monetization strategies, lead nurturing tactics and detailed analytics and reporting.
 
-We are not just a vendor, but a partner in your success. We work closely with you to help you achieve your business goals, whether it's expanding your customer base, increasing sales, or building your brand.
+We are not just a vendor, but a partner in your success. We work closely with you to help you achieve your business goals, whether its expanding your customer base, increasing sales, or building your brand.
 
 At LeadPro Media LLC, we are committed to delivering results that exceed your expectations. We invite you to contact us today for a consultation, and let us show you how we can take your business to the next level with our cutting-edge lead generation and online monetization strategies.
             </div>
